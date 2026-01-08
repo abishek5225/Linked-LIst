091a6f475a18d4957056ba0881a9584b27da0a9e
Abishek CHaulagain <abishekchaulagain5225@gmail.com>
2026-01-08 22:10:19 +0545

    singly linked list

--- Singly-Linked-List/singly.c
+++ Singly-Linked-List/singly.c
@@ -20,3 +20,57 @@ void insertAtBeggining(struct Node** head, int value) {
     newNode->next = *head;
     *head = newNode;
 }
+
+void insertAtEnd(struct Node** head, int value){
+    struct Node* newNode = createNode(value);
+    if(*head == NULL){
+        *head = newNode;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void deleteNode(struct Node** head, int value){
+    struct Node* temp= *head;
+    struct Node* prev = NULL;
+
+    if(temp != NULL && temp->data == value){
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while(temp != NULL && temp->data != value){
+        prev = temp;
+        temp = temp->next;
+    }
+    if(temp == NULL){
+        return;
+    }
+    
+    prev->next = temp->next;
+    free(temp);
+}
+
+void printList(struct Node* head){
+    while(head != NULL){
+        printf("%d -> ", head->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    insertAtBeggining(&head, 10);
+    insertAtBeggining(&head, 20);
+    insertAtEnd(&head, 30);
+    insertAtEnd(&head, 40);
+
+    printList(head);
+}
