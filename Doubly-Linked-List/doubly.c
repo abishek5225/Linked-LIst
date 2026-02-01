128952cf00b7b1283820414bf9f86beb0298ad32
Abishek CHaulagain <abishekchaulagain5225@gmail.com>
2026-02-01 19:33:00 +0545

    updated

--- Doubly-Linked-List/doubly.c
+++ Doubly-Linked-List/doubly.c
@@ -8,8 +8,12 @@ struct Node{
 }
 
 struct Node* createNode(int value){
-    struct Node* newNode = malloc(sizeof(struct Node));
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
 
+
+        if(newNode == NULL){
+            printf("Memory allocation failed\n");
+        }
     newNode->data = value;
     newNode->prev = NULL;
     newNode->next = NULL;
@@ -26,7 +30,40 @@ void insertAtBegginning(struct Node** head, int value){
         *head = newNode;
         return;
     }
-    newNode-next= *head;
+    newNode->next= *head;
     (*head)->prev = newNode;
     *head = newNode;
+}
+
+void insertAtEnd(struct Node **head, int value){
+    struct Node *newNode = createNode(value);
+    struct Node *temp = *head;
+
+    if(*head == NULL){
+        *head = newNode;
+        return;
+    }
+
+    while(temp->next !=NULL){
+        temp = temp->next;
+    }
+
+     temp->next = newNode;
+     newNode->prev = temp;
+}
+
+void displayBackward(struct Node *head){
+    struct Node *temp = head;
+
+    if (temp == NULL) return;
+
+    while(temp->next != NULL){
+        printf("%d <-> ", temp->data);
+        temp = temp->prev;
+    }
+    printf("Null\n");
+}
+
+void deleteNode(struct Node **head, int value){
+    
 }
